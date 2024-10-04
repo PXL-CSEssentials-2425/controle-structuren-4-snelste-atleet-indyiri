1774a06ed21e0f1ebe658f230cde71400f0f5d2c
Indy Iri <indy.iri@student.pxl.be>
2024-10-04 12:26:40 +0200

    Oef 4  resultTextBox bijna af

--- H3Oef4SnelsteAtleet/H3Oef4SnelsteAtleet/MainWindow.xaml.cs
+++ H3Oef4SnelsteAtleet/H3Oef4SnelsteAtleet/MainWindow.xaml.cs
@@ -20,5 +20,42 @@ namespace H3Oef4SnelsteAtleet
         {
             InitializeComponent();
         }
+
+        string nameFastest;
+        int timeFastest = 0;
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            int timeCurrent;
+            bool isValidNumber = int.TryParse(timeTextBox.Text, out timeCurrent);
+
+            if (isValidNumber == true)
+            //of 'if (isValidNumber)' is eigenlijk al genoeg 
+            {
+                if ( timeFastest == 0 || timeCurrent < timeFastest)
+                {
+                    timeFastest = timeCurrent;
+                    nameFastest = nameTextBox.Text;
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTextBox.Text = string.Empty;
+            timeTextBox.Text = string.Empty;
+            resultTextBox.Text = string.Empty;
+        }
+
+        private void fastestButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Text = $"De snelste atleet is {nameFastest} \n\rTotaal aantal seconden: {timeFastest}\n\r\n\rAantal uren: \n\rAantal minuten: \n\rAantal seconden";
+            
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
